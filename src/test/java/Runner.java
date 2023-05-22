e5f399104727c2c47f77db6fa53421c9f2a3b884
0x4rchange1 <whistler.20463793@gmail.com>
2023-05-22 12:41:27 +0530

    test5

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,13 +1,14 @@
-package bdd_runner;
+
 
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "/Users/mk/eclipse-workspace/Irctc-project/src/main/java/bdd/petstore.feature", glue = {
 		"bdd_bindings" })
-public class Runner {
+public class Runner extends AbstractTestNGCucumberTests{
 
 }
